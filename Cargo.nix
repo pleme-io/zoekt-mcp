1c49eaa466555b4cba272de88884ad2cbba1b9f8
drzzln <drzzln@protonmail.com>
2026-05-21 00:10:01 -0400

    fix(Cargo.nix): SRI hash for shikumi@5db7873 (crate2nix drift)

--- Cargo.nix
+++ Cargo.nix
@@ -5309,7 +5309,7 @@ rec {
         src = pkgs.fetchgit {
           url = "https://github.com/pleme-io/shikumi";
           rev = "5db7873c9a109f619474228cbea15d64b4825b8a";
-          sha256 = "LJrkEo5yMS2OqfM7vsZje+uBfB6z4rfO6ZGzFzQTNdo=";
+          sha256 = "sha256-LJrkEo5yMS2OqfM7vsZje+uBfB6z4rfO6ZGzFzQTNdo=";
         };
         dependencies = [
           {
